2c62421a7c2405ddca598b1bc3e0f1d675594d44
btapo <bhatt.tapo@gmail.com>
2021-06-04 19:32:47 +0530

    Application based layout along with dependencies

---
+++ src/main/java/com/btapo/interview/screeningbmireport/ScreeningBmiReportApplication.java
@@ -0,0 +1,13 @@
+package com.btapo.interview.screeningbmireport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningBmiReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreeningBmiReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btapo/interview/screeningbmireport/ScreeningBmiReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btapo.interview.screeningbmireport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreeningBmiReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
